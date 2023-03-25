81bab54f1c295b78bf298448d268ae0ab0ca7e7c
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-24 22:17:48 -0400

    This is a temperature converter from kelvin to celsius, newton, and fahrenheit

---
+++ temperatureConverter.js
@@ -0,0 +1,23 @@
+// Declared constant variable kelvin, assigned to value
+let kelvin = 293;
+
+
+// Declared constant variable celsius, assigned to value
+let celsius = kelvin - 273;
+
+// Declared constant variable fahrenheit, assigned to value
+let fahrenheit = celsius * (9 / 5) + 32;
+
+//
+let newton = celsius * (33 / 100);
+
+// Reassigned fahrenheit variable to lowest decimal number
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
+
+newton = (celsius * 33) / 100;
+console.log(newton);
+
+newton = Math.floor(newton);
+console.log(`The `);
